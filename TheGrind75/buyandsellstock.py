898c47a2fcd9186835f928bf1370ba4d504ff564
nav272 <nav272@nyu.edu>
2022-07-25 11:10:46 +0530

    update buyandsellstock.py

--- TheGrind75/buyandsellstock.py
+++ TheGrind75/buyandsellstock.py
@@ -1,10 +1,12 @@
 def maxProfit(prices):
-    min_price =prices[0]
-		max_profit = 0
+    min_price = prices[0]
+    max_profit = 0
     for price in prices:
         if price < min_price:
-						min_price = price
+            min_price = price
         profit = price - min_price
-				if profit > max_profit
-		        max_profit = profit
+        if profit > max_profit:
+            max_profit = profit
     return max_profit
+
+print(maxProfit([1,2,3,4,5]))
