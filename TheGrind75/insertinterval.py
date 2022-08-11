c6b25475077c46da302625b46f8a781b67a578b5
nav272 <nav272@nyu.edu>
2022-08-11 21:56:44 +0530

    update insertinterval.py

--- TheGrind75/insertinterval.py
+++ TheGrind75/insertinterval.py
@@ -1,21 +1,24 @@
-class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        result = []
-        
-        for index, interval in enumerate(intervals):
-			# the new interval and the current interval don't overlap
-            if interval[1] < newInterval[0]:
-                result.append(interval)
-            # the new interval's range is before the current interval
-            elif interval[0] > newInterval[1]:
-                result.append(newInterval)
-                newInterval = interval
-                return result+intervals[index:]
-            # in case of overlap
-            elif interval[1] >= newInterval[0] or interval[0] <= newInterval[1]:
-                newInterval[0] = min(interval[0], newInterval[0])
-                newInterval[1] = max(newInterval[1], interval[1])
+import random
 
-        
-        result.append(newInterval); 
-        return result
+class Solution:
+    def insertIntervals(intervals, newInterval):
+        results = []
+        startTimePosition = 0
+        endTimePosition = 1
+        for index, interval in enumerate(intervals):
+            #first condition where the current interval's end position is less than newInterval's start position, just add the current interval as is.
+            if interval[endTimePosition] < newInterval[startTimePosition]:
+                results.append(interval)
+            #second condition where the current interval's start is greater than newInterval's end, just add the new interval and return the rest of the intervals list
+            elif interval[startTimePosition] > newInterval[endTimePosition]:
+                print("2nd one")
+                results.append(newInterval)
+                return results + intervals[index:]
+            #third condition is when there is an overlap, end of current interval is greater than or equal to start of the new Interval or start of the current interval is less than or equal to the end of the new Interval
+            else:
+                newInterval[startTimePosition] = min(newInterval[startTimePosition], interval[startTimePosition])
+                newInterval[endTimePosition] = max(newInterval[endTimePosition], interval[endTimePosition])
+        results.append(newInterval)
+        return results
+
+print(Solution.insertIntervals([[1,3],[6,9]], [3,7]))
