46a9767067654cc5c18856a25e1f84345c0acb30
nav272 <nav272@nyu.edu>
2022-09-16 02:03:10 +0530

    create findallanagrams2.py

---
+++ TheGrind75/findallanagrams2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findAnagrams(self, s, p):
+        n = len(s)
+        required = len(p)
+        if required > n:
+            return []
+        alphabets = "abcdefghijklmnopqrstuvwxyz"
+        alphabetsHash = {}
+        for i in alphabets:
+            alphabetsHash[i] = hash(i)
+        left = 0
+        start = len(p)-1
+        end = len(s)
+        answer = []
+        hashp = sum(map(hash,p))
+        #print(hashp)
+        current = sum(map(hash, s[:len(p)-1]))
+        for right in range(start, end):
+            current += alphabetsHash[s[right]]
+            if current == hashp:
+                answer.append(left)
+            current -= alphabetsHash[s[left]]
+            left +=1
+        return answer
+
+print(Solution().findAnagrams("ababababab", "aab"))
+
